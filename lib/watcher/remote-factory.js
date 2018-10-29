gyselroth/balloon-node-sync

commit e1a8e549f12f249c375e11a019136ef96ee5f594

Improve error handling of watcher

lib/watcher/remote-factory.js
@@ -36,8 +36,9 @@ RemoteWatcherFactory.prototype.start = function() {
   this.pollIntervall = setInterval(() => {
     blnApi.nodeDelta({limit: 1, cursor: lastCursor.read()}, (err, data) => {
       if(err) {
-        logger.error('Recievieng node delta had error', {category: 'sync.watcher.remote', err});
-        this.emit('error', err);
+        var errToEmit = new BlnWatcherError('Recievieng node delta had error', 'E_BLN_REMOTE_WATCHER_DELTA');
+        errToEmit.origErr = err;
+        this.emit('error', errToEmit);
         return;
       }
 

errors/bln-watcher.js
@@ -1,0 +1,14 @@
+/**
+ * Custom errors for watcher
+ *
+ * @param {string} message - error message
+ * @param {string} [code] - error code. Default: `E_BLN_WATCHER_ERROR`
+ */
+module.exports = function BlnWatcherError(message, code) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = message;
+  this.code = code || 'E_BLN_WATCHER_ERROR';
+};
+
+require('util').inherits(module.exports, Error);

lib/watcher/local-factory.js
@@ -10,6 +10,8 @@ const ignoreDb = require('../ignore-db.js');
 const logger = require('../logger.js');
 const utility = require('../utility.js');
 
+var BlnWatcherError = require('../../errors/bln-watcher.js');
+
 const STATE_INITIAL = 0;
 const STATE_STARTING = 1;
 const STATE_STARTED = 2;
@@ -89,7 +91,17 @@ LocalWatcherFactory.prototype.start = function() {
   {
     debounceMS: 1000,
     errorCallback: (error) => {
-      this.emit('error', error);
+      var err;
+
+      switch(error) {
+        case 'Service shutdown unexpectedly':
+          err = new BlnWatcherError('Local watcher shutdown unexpectedly', 'E_BLN_LOCAL_WATCHER_SHUTDOWN');
+        break;
+        default:
+          err = new BlnWatcherError('Unexpected error in local watcher', 'E_BLN_LOCAL_WATCHER_UNEXPECTED');
+      }
+
+      this.emit('error', err);
     }
   })
   .then((watcher) => {

sync-watcher-factory.js
@@ -132,9 +132,9 @@ SyncFactory.prototype._initializeLocalWatcher = function() {
   });
 
   this.localWatcher.on('error', (err) => {
-    // TODO pixtron - handle errors
     logger.error('Local watcher had error', {category: 'watcher', err});
-    throw err;
+
+    this.emit('error', err);
   });
 }
 
@@ -152,8 +152,8 @@ SyncFactory.prototype._initializeRemoteWatcher = function() {
   });
 
   this.remoteWatcher.on('error', (err) => {
-    // TODO pixtron - handle errors
     logger.error('Remote watcher had error', {category: 'watcher', err});
-    throw err;
+
+    this.emit('error', err);
   });
 }

commit 11dd77606ee03324c15aef857ca85f6e54ef7775

Fixes regression introduced with e1a8e54

lib/watcher/remote-factory.js
@@ -6,6 +6,8 @@ const config = require('../config.js');
 const lastCursor = require('../last-cursor.js');
 const logger = require('../logger.js');
 
+const BlnWatcherError = require('../../errors/bln-watcher.js');
+
 const STATE_INITIAL = 0;
 const STATE_STARTED = 1;
 const STATE_STOPED = 2;