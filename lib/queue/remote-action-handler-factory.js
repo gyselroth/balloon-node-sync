gyselroth/balloon-node-sync

commit 7ddc5a8dc7f0b9329326e8906a9838dd13b4050d

Make error messages more readable

lib/queue/remote-action-handler-factory.js
@@ -9,7 +9,8 @@ var utility = require('../utility.js');
 var logger = require('../logger.js');
 
 function handleError(err, task, callback) {
-  logger.error('REMOTEQUEUEHANDLER: Task had error, rescheduling it for retry', {task, err});
+  const errorMessage = `${err.message} for '${utility.joinPath(task.node.parent, task.node.name)}'`;
+  logger.error(errorMessage, {category: 'remote-action-handler', code: err.code});
   queueErrorDb.insert({err, task, origin: 'remote'}, callback);
 }
 
@@ -113,7 +114,8 @@ module.exports = function(actionQueue, transferQueue) {
               } else {
                 if(task.priority === 6 )  {
                   //parent does still not exist, log error
-                  logger.error('Trying to move node to an inexistent parent node', {category: 'sync.queue.remoteActionHandler', node, newName});
+                  var errorMessage = `Trying to move node '${newName}' to an inexistent parent node '${utility.joinPath(dbNode.parent, dbNodes.name)}'`;
+                  logger.error(errorMessage, {category: 'sync.queue.remoteActionHandler', node, newName});
                   return done();
                 }
 

lib/bln-api-request.js
@@ -94,21 +94,20 @@ function handleResponse(response, reqOptions, body, callback) {
   if(response.statusCode === 401) {
     logger.warning('API: Got Status Code 401 Unauthorized');
 
-    return callback(new BlnApiRequestError('API: User is not authenticated', 'E_BLN_API_REQUEST_UNAUTHORIZED'));
+    return callback(new BlnApiRequestError('User is not authenticated', 'E_BLN_API_REQUEST_UNAUTHORIZED'));
   }
 
   if(response.statusCode < 200 || response.statusCode > 299) {
     var errCode;
     var errMsg = 'Unknown API Error with status code: ' + response.statusCode;
 
-
     if(body && (body.error || body.message || body.code)) {
-      if(body.error) {
-        errMsg = body.error;
+      if(body.message) {
+        errMsg = body.message;
       }
 
-      if(body.message) {
-        errMsg = (errMsg ? (errMsg + ' ') : '') + '(' + body.message + ')';
+      if(body.error) {
+        errMsg = (errMsg ? (errMsg + ' ') : '') + '(' + body.error + ')';
       }
 
       if(body.code) {

lib/delta/delta.js
@@ -317,7 +317,7 @@ function resolveFileConflicts(node, callback) {
       localHash = fsWrap.md5FileSync(currentLocalPath);
       stat = fsWrap.lstatSync(currentLocalPath);
     } catch(e) {
-      logger.error('resolveFileConflicts failed', {category: 'sync-delta', node, err: e});
+      logger.warning('resolveFileConflicts failed', {category: 'sync-delta', node, err: e});
 
       var err = new BlnDeltaError(e.message);
       throw err;
@@ -415,12 +415,12 @@ function findLocalConflict(node, callback) {
 
       return syncDb.findByIno(targetStat.ino, (err, syncedNode) => {
         if(err) {
-          logger.error('DELTA: database error:', {err});
+          logger.warning('database error', {category: 'sync-delta', err});
           throw (new BlnDeltaError('Database error'));
         }
 
         if(!syncedNode) {
-          logger.error('DELTA: node not found in database even if it should exists:', {targetPath, node, targetStat});
+          logger.warning('node not found in database even if it should exists:', {category: 'sync-delta', targetPath, node, targetStat});
           throw (new BlnDeltaError('Target node \' ' + targetPath + ' \' with ino \'' + targetStat.ino + '\' not found in db'));
         }
 
@@ -504,7 +504,7 @@ function renameRemoteNode(node, name, callback) {
     try {
       fsWrap.renameSync(oldLocalPath, newLocalPath);
     } catch(err) {
-      logger.error('rename local node failed', {category: 'sync-delta', oldLocalPath, newLocalPath, err})
+      logger.warning('rename local node failed', {category: 'sync-delta', oldLocalPath, newLocalPath, err})
       throw new BlnDeltaError(err.message);
 
       return;
@@ -518,7 +518,7 @@ function renameRemoteNode(node, name, callback) {
 
   return blnApi.renameNode({remoteId, name: node.name}, (err) => {
     if(err) {
-      logger.error('rename remote node failed', {category: 'sync-delta', params: {remoteId, name: node.name}, err})
+      logger.warning('rename remote node failed', {category: 'sync-delta', params: {remoteId, name: node.name}, err});
       throw new BlnDeltaError(err.message);
 
       return;

lib/delta/local-delta.js
@@ -106,7 +106,7 @@ var localDelta = {
     fsWrap.readdir(dirPath, (err, nodes) => {
       //if it is not possible to read dir abort sync
       if(err) {
-        logger.error('Error while reading dir', {category: 'sync-local-delta', dirPath, err});
+        logger.warning('Error while reading dir', {category: 'sync-local-delta', dirPath, err});
         throw new BlnDeltaError(err.message);
       }
 
@@ -116,7 +116,7 @@ var localDelta = {
         try {
           var stat = fsWrap.lstatSync(nodePath);
         } catch(e) {
-          logger.error('LOCAL DELTA: findChanges: got lstat error on node', {category: 'sync-local-delta', nodePath, code: e.code});
+          logger.warning('findChanges got lstat error on node', {category: 'sync-local-delta', nodePath, code: e.code});
           throw new BlnDeltaError(e.message);
         }
 
@@ -261,7 +261,7 @@ var localDelta = {
       try {
         var stat = fsWrap.lstatSync(nodePath);
       } catch(e) {
-        logger.error('LOCAL DELTA: findDeletedNodes: got lstat error on node', {category: 'sync-local-delta', nodePath, code: e.code});
+        logger.warning('findDeletedNodes got lstat error on node', {category: 'sync-local-delta', nodePath, code: e.code});
         throw new BlnDeltaError(e.message);
       }
 

lib/fs-wrap.js
@@ -237,7 +237,8 @@ var fsWrap = {
         var srcExists = fs.existsSync(tempPath);
         var targetParentExists = this.existsSync(path.join(targetPath, '..'));
 
-        logger.error('FS: moveTempFile had error', {code: err.code, message: err.message, targetParentExists, srcExists});
+        var errorMessage = `Could not move temp file '${tempPath}' to Balloon '${targetPath}'`;
+        logger.error(errorMessage, {code: err.code, message: err.message, targetParentExists, srcExists});
         throw err;
       }
     }

lib/queue/local-action-handler-factory.js
@@ -18,7 +18,8 @@ module.exports = function(actionQueue, transferQueue) {
   var readonlyConflictHandler = readonlyConflictHandlerFactory(actionQueue, transferQueue);
 
   function handleError(err, task, addToErrorQueue, callback) {
-    logger.error('LOCALQUEUEHANDLER: ' + err.message, err.code);
+    const errorMessage = `${err.message} for '${utility.joinPath(task.node.parent, task.node.name)}'`;
+    logger.error(errorMessage, {category: 'local-action-handler', code: err.code});
 
     if(addToErrorQueue) {
       logger.info('LOCALQUEUEHANDLER: rescheduling task with error', task);

lib/queue/transfer-queue-factory.js
@@ -74,7 +74,8 @@ module.exports = function(actionQueue) {
                 return done(null);
               break;
               default:
-                logger.error('filedownload failed', {category: 'transfer-queue', message: err.message, code: err.code});
+                var errorMessage = `file download for node with id '${node.remoteActions.create.remoteId}' failed`;
+                logger.error(errorMessage, {category: 'transfer-queue', message: err.message, code: err.code});
                 emitStateEvent(task, 'error', err);
                 return queueErrorDb.insert({err, task, origin: 'remote'}, done);
               break;
@@ -158,7 +159,8 @@ module.exports = function(actionQueue) {
                 return done(null);
               break;
               default:
-                logger.error('Fileupload failed', {category: 'transfer-queue', message: err.message, code: err.code});
+                var errorMessage = `Upload of '${utility.joinPath(node.parent, node.name)}' failed`;
+                logger.error(errorMessage, {category: 'transfer-queue', message: err.message, code: err.code});
                 emitStateEvent(task, 'error', err);
                 return queueErrorDb.insert({err, task, origin: 'local'}, done);
               break;
@@ -176,7 +178,7 @@ module.exports = function(actionQueue) {
                   return done(null);
                 break;
                 default:
-                  logger.error('getAttributes failed: unauthorized', {category: 'transfer-queue', message: err.message, code: err.code});
+                  logger.warning('getAttributes failed: unauthorized', {category: 'transfer-queue', message: err.message, code: err.code});
                   return done(null);
                 break;
               }

lib/selective.js
@@ -232,7 +232,7 @@ var selective = {
           try {
             var stat = fsWrap.lstatSync(nodePath);
           } catch(e) {
-            logger.error('Got lstat error', {category: 'sync.selective', nodePath, code: e.code});
+            logger.warning('Got lstat error', {category: 'sync.selective', nodePath, code: e.code});
             return cb(e);
           }
 

commit 22fa3b978f2e2fc32336673c936287f0fb733980

Fix regression introduced with 7ddc5a8

lib/queue/remote-action-handler-factory.js
@@ -114,7 +114,7 @@ module.exports = function(actionQueue, transferQueue) {
               } else {
                 if(task.priority === 6 )  {
                   //parent does still not exist, log error
-                  var errorMessage = `Trying to move node '${newName}' to an inexistent parent node '${utility.joinPath(dbNode.parent, dbNodes.name)}'`;
+                  var errorMessage = `Trying to move node '${newName}' to an inexistent parent node`;
                   logger.error(errorMessage, {category: 'sync.queue.remoteActionHandler', node, newName});
                   return done();
                 }