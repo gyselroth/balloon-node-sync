gyselroth/balloon-node-sync

commit 32c80ce97388515633757065aedf149608a2e23e

Improve ignoring shares by default
Related to: gyselroth/balloon-client-desktop/issues/162

sync-watcher-factory.js
@@ -43,9 +43,9 @@ SyncFactory.prototype.start = function() {
       ignoreDb.connect(config.get('instanceDir'), cb);
     },
     (cb) => {
-      logger.debug('Update ignore db', {category: 'sync.watcher'});
+      logger.debug('Update renote paths in ignore db', {category: 'sync.watcher'});
 
-      selective.updateIgnoreDb(cb);
+      selective.updateRemotePaths(cb);
     },
     (cb) => {
       this.localWatcher.once('started', cb);

full-sync-factory.js
@@ -22,6 +22,7 @@ var syncDb = require('./lib/sync-db.js');
 var syncEvents = require('./lib/sync-events.js')();
 var transferDb = require('./lib/transfer-db.js');
 var utility = require('./lib/utility.js');
+var knownSharesDb = require('./lib/known-shares-db.js');
 
 var BlnConfigError = require('./errors/bln-config.js');
 
@@ -66,25 +67,20 @@ SyncFactory.prototype.updateSelectiveSync = function(newIgnoredIds) {
   });
 }
 
-SyncFactory.prototype.initializeIgnoreDb = function(nodes) {
-  return new Promise((resolve, reject) => {
-    this.connectDbs({transferDb: true, queueErrorDb: true, sybcDb: true}, err => {
-      if(err) return reject(err);
-
-      selective.initializeIgnoreDb(nodes, (err) => {
-        if(err) return reject(err);
-
-        resolve();
-      });
-    });
-  });
-}
-
 SyncFactory.prototype.getIgnoredRemoteIds = function(callback) {
   this.connectDbs({transferDb: true, queueErrorDb: true, syncDb: true}, (err) => {
     if(err) return callback(err);
 
     selective.getIgnoredRemoteIds(callback);
+  });
+}
+
+
+SyncFactory.prototype.ignoreNewShares = function(callback) {
+  this.connectDbs({transferDb: true, queueErrorDb: true, syncDb: true}, (err) => {
+    if(err) return callback(err);
+
+    selective.ignoreNewShares(callback);
   });
 }
 
@@ -585,6 +581,14 @@ SyncFactory.prototype.connectDbs = function(excludeDbs, callback) {
       if(this.stopped) return cb(null);
 
       ignoreDb.connect(instanceDir, cb);
+    },
+    (cb) => {
+      if(excludeDbs.knownSharesDb) return cb(null);
+
+      logger.debug('Connecting knownSharesDb', {category: 'sync.main', stopped: this.stopped});
+      if(this.stopped) return cb(null);
+
+      knownSharesDb.connect(instanceDir, cb);
     },
     (cb) => {
       if(excludeDbs.queueErrorDb) return cb(null);

lib/bln-api.js
@@ -429,7 +429,7 @@ var blnApi = {
   },
 
   nodeDelta: function(params, callback) {
-    var requestParams = {attributes: ['parent', 'hash', 'version', 'size', 'shared', 'reference']};
+    var requestParams = {attributes: ['parent', 'hash', 'version', 'size']};
 
     if(!!(params && params.constructor && params.call && params.apply)) {
       callback = params;

lib/delta/remote-delta.js
@@ -44,30 +44,7 @@ function fetchDelta(params, callback) {
         });
       },
       (cb) => {
-        async.series([
-          (cbSeries) => {
-            async.each(data.nodes, (node, cbEach) => {
-              // if params.id is set, this is a forced download of a collection,
-              // hence do not ignore the node (eg. after unignoring a share)
-              if(node.shared === true && node.reference === true && !params.id) {
-                logger.info('ignoring new share', {category: 'sync-remote-delta', node});
-                ignoreDb.add({remoteId: node.id, remotePath: node.path, path: node.path}, (err) => {
-                  if(err) {
-                    logger.warning('Could not ignore new share', {category: 'sync-remote-delta', err});
-                    return cbEach(err);
-                  }
-
-                  cbEach(null);
-                });
-              } else {
-                cbEach(null);
-              }
-            }, cbSeries);
-          },
-          (cbSeries) => {
-            groupDelta(data.nodes, cbSeries);
-          }
-        ], cb);
+        groupDelta(data.nodes, cb);
       }
     ], (err, results) => {
       if(err) return callback(err);

lib/known-shares-db.js
@@ -1,0 +1,45 @@
+var path = require('path');
+var nedb = require('nedb');
+
+var utility = require('./utility.js');
+var connected = false;
+
+var knownSharesDb = {
+  isConnected: function() {
+    return connected;
+  },
+
+  getCollectionPath: function(dbPath) {
+    return path.join(dbPath, 'known-shares.db');
+  },
+
+  connect: function(dbPath, callback) {
+    var pathCollection = this.getCollectionPath(dbPath);
+
+    this.db = new nedb({
+      filename: pathCollection,
+      autoload: true,
+      onload: (err) => {
+        callback(err);
+      }
+    });
+
+    this.db.ensureIndex({fieldName: 'remoteId'});
+
+    connected = true;
+  },
+
+  getDb: function() {
+    return this.db;
+  },
+
+  find: function(query, callback) {
+    this.db.find(query, callback);
+  },
+
+  insert: function(node, callback) {
+    this.db.insert(node, callback);
+  }
+}
+
+module.exports = knownSharesDb;

lib/selective.js
@@ -8,23 +8,9 @@ var ignoreDb = require('./ignore-db.js');
 var logger = require('./logger.js');
 var syncDb = require('./sync-db.js');
 var utility = require('./utility.js');
-
-
+var knownSharesDb = require('./known-shares-db.js');
 
 var selective = {
-  /**
-   * Initializes ignoreDb with a set of remote nodes
-   *
-   * @param {Array} nodes - array of nodes to ignore
-   * @param {Function} callback - callback function
-   * @returns {void}
-   */
-  initializeIgnoreDb(nodes, callback) {
-    async.map(nodes, (node, cb) => {
-      ignoreDb.add({remoteId: node.id, path: node.path}, cb);
-    }, callback);
-  },
-
   /**
    * Gets all currently ignored remote id's as an array and passes it to the callback
    *
@@ -46,7 +32,7 @@ var selective = {
     const instanceDirPath = config.get('instanceDir');
     const self = this;
 
-    this.updateIgnoreDb(err => {
+    this.updateRemotePaths(err => {
       if(err) return callback(err);
 
       const rootNode = difference['#'];
@@ -171,12 +157,25 @@ var selective = {
   },
 
   /**
-   * Updates remote paths of all ignored nodes
+   * Updates remote paths and ignored shares
    *
    * @param {Function} callback - callback function
    * @returns {void}
    */
   updateIgnoreDb: function(callback) {
+    async.parallel([
+      this.updateRemotePaths,
+      this.ignoreNewShares
+    ], callback);
+  },
+
+  /**
+   * Updates remote paths of all ignored nodes
+   *
+   * @param {Function} callback - callback function
+   * @returns {void}
+   */
+  updateRemotePaths: function(callback) {
     ignoreDb.getIgnoredRemoteIds((err, ignoredRemoteIds) => {
       if(err) return callback(err);
 
@@ -187,6 +186,57 @@ var selective = {
       });
     });
   },
+
+  /**
+   * Ignores all new shares by default
+   *
+   * @param {Function} callback - callback function
+   * @returns {void}
+   */
+  ignoreNewShares: function(callback) {
+    logger.info('Ignore new shares', {category: 'sync.selective'});
+
+    async.waterfall([
+      (cb) => {
+        async.parallel([
+          (cbParallel) => {
+            knownSharesDb.find({}, cbParallel);
+          },
+          (cbParallel) => {
+            blnApi.queryNodes({shared:true, reference:{'$exists':true}}, cbParallel);
+          }
+        ], cb);
+      },
+      (results, cb) => {
+        const [knownShares, nodes] = results;
+
+        async.map(nodes, (node, cbMap) => {
+          if(knownShares.find(share => (share.remoteId === node.id))) {
+            cbMap(null);
+          } else {
+            logger.info('ignoring new share', {category: 'sync.selective', node});
+
+            async.parallel([
+              (cbParallel) => {
+                ignoreDb.add({remoteId: node.id, remotePath: node.path, path: node.path}, cbParallel);
+              },
+              (cbParallel) => {
+                knownSharesDb.insert({remoteId: node.id}, cbParallel);
+              }
+            ], cbMap);
+          }
+        }, cb);
+      }
+    ], (err) => {
+      if(err) {
+        logger.error('Could not ignore new shares', {category: 'sync.selective', err});
+        return callback(err);
+      }
+
+      callback(null);
+    });
+  },
+
 
   /**
    * Removes an ignored node localy

tests/integration/mocks/bln-api-factory.js
@@ -208,6 +208,10 @@ module.exports = function(pathFixtures) {
       var deltaResult = require(path.join(pathFixtures, deltaNameParts.join('-') + '.json'));
 
       callback(null, deltaResult);
+    },
+
+    queryNodes: function(query, callback) {
+      return callback(null, []);
     }
   }
 

tests/integration/mocks/known-shares-db-factory.js
@@ -1,0 +1,96 @@
+var fs = require('fs');
+var path = require('path');
+var extend = require('util')._extend;
+var nedb = require('nedb');
+
+var async = require('async');
+
+var utility = require('../../../lib/utility.js');
+
+var databasePath = path.join(__dirname, 'db');
+var pathTmpCollectionFile = path.join(databasePath, 'known-shares-db.db');
+var nextAutoIncrement = 0;
+var db;
+
+module.exports = function(pathFixtures) {
+  var knownSharesDb = {
+    isConnected: function() {
+      return true;
+    },
+
+    getCollectionPath: function(dbPath) {
+      return path.join(pathFixtures, 'local-known-shares-db.json');
+    },
+
+    connect: function(dbPath, callback) {
+      var pathCollection = this.getCollectionPath(dbPath);
+      var actualDb = [];
+
+      if(fs.existsSync(pathCollection)) {
+        actualDb = require(pathCollection);
+      }
+
+      nextAutoIncrement = actualDb.length;
+
+      if(fs.existsSync(databasePath) === false) {
+        fs.mkdirSync(databasePath);
+      }
+
+      if(fs.existsSync(pathTmpCollectionFile)) {
+        fs.unlinkSync(pathTmpCollectionFile);
+      }
+
+      this.db = db = new nedb({
+        filename: pathTmpCollectionFile,
+        autoload: true,
+        onload: (err) => {
+          async.mapLimit(actualDb, 5, (node, cb) => {
+            this.db.insert(node, cb);
+          }, (err, results) => {
+            callback(err);
+          });
+        }
+      });
+    },
+
+    insert: function(node, callback) {
+      node._id = getAutoIncrement();
+
+      this.db.insert(node, callback);
+    },
+  }
+
+  function getAutoIncrement() {
+    var autoIncrement = nextAutoIncrement+"";
+    nextAutoIncrement++;
+
+    return autoIncrement;
+  }
+
+  return {
+    mock: knownSharesDb,
+    tearDown: function() {
+      if(fs.existsSync(pathTmpCollectionFile)) {
+        fs.unlinkSync(pathTmpCollectionFile);
+      }
+
+      if(fs.existsSync(databasePath) === false) {
+        fs.rmdirSync(databasePath);
+      }
+
+      db = undefined;
+      nextAutoIncrement = 0
+    },
+    getNodeList: function(callback) {
+      return db.find({}).sort({_id: 1}).exec((err, docs) => {
+        var nodes = [];
+
+        if(docs) {
+          nodes = docs;
+        }
+
+        callback(null, nodes);
+      });
+    }
+  };
+};

commit a016bf81d2e05b7684a7666d67d741743d06c3ce

Fix regression introduced with 32c80ce

sync-watcher-factory.js
@@ -151,7 +151,7 @@ SyncFactory.prototype._initializeRemoteWatcher = function() {
   this.remoteWatcher.on('changes', (changes) => {
     logger.debug('remote watcher emmited changes', {category: 'sync.watcher', changes, paused: this.paused})
 
-    selective.updateIgnoreDb(err => {
+    selective.updateRemotePaths(err => {
       if(this.paused === false) {
         this.emit('changed', 'remote');
       }