0b97520697a34a72458b22399ab2f77478479c0c
Stefan Aebischer <aebischer@pixtron.ch>
2020-01-10 13:01:22 -0500

    Update winston to v3.2
    Resolves: #33

--- tests/integration/lib/logger-factory.js
+++ tests/integration/lib/logger-factory.js
@@ -1,25 +1,30 @@
 var path = require('path');
 
 var winston = require('winston');
+var TransportStream = require('winston-transport');
 
 var logLevels = {error: 3, warning: 4, notice: 5, info: 6, debug: 7};
 var logger;
+
+class NullTransport extends TransportStream {
+  constructor(options = {}) {
+    super(options);
+
+    this.name = 'null'
+  }
+
+  log(info, callback) {
+    callback(null, true);
+  }
+}
 
 module.exports = function(config) {
   if(!logger) {
     var pathLogFile = path.join(config.configDir, 'error.log');
 
-    logger = new (winston.Logger)({levels: logLevels});
+    logger = winston.createLogger({levels: logLevels});
 
-    /*
-    logger.add(winston.transports.Console, {
-      level: 'debug',
-      prettyPrint: true,
-      depth: 6,
-      humanReadableUnhandledException: true
-    });
-    */
-
+    logger.add(new NullTransport());
   }
 
   return logger;
